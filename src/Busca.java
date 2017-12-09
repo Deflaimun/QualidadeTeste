5c29aef43da6e9b49023337905c841811d94b940
Paulo.borges <paulohtb@hotmail.com>
2017-12-09 02:28:52 -0200

    Busca descentendente funcionando com 100% de cobertura de código

--- src/Busca.java
+++ src/Busca.java
@@ -25,7 +25,7 @@ public class Busca {
                     }
                 }
 
-                if (type== SearchType.GreaterThanEquals) {
+                if (type == SearchType.GreaterThanEquals) {
                     if(key == array[i]){
                         retorno.setSearchResult(SearchResult.FoundExact);
                         return retorno;
@@ -71,6 +71,66 @@ public class Busca {
                     }
                 }
             }
+        } else{
+            for (int i = size-1; i >= 0; i--) {
+                retorno.setResultIndex(i);
+                retorno.setResultValue(key);
+
+                if(type == SearchType.Equals){
+                    if(key == array[i]) {
+                        retorno.setSearchResult(SearchResult.FoundExact);
+                        return retorno;
+                    }
+                }
+
+                if (type== SearchType.GreaterThanEquals) {
+                    if(key == array[i]){
+                        retorno.setSearchResult(SearchResult.FoundExact);
+                        return retorno;
+                    }
+
+                    else if(array[i] > key){
+                        retorno.setResultValue(array[i]);
+                        retorno.setSearchResult(SearchResult.FoundGreater);
+                        return retorno;
+                    }
+                }
+
+                if (type == SearchType.LessThanEquals) {
+                    if (key >= array[size-1]) {
+                        if (key == array[i]) {
+                            retorno.setSearchResult(SearchResult.FoundExact);
+                            return retorno;
+
+                        } else if (array[i] > key) {
+                            retorno.setResultIndex(i+1);
+                            retorno.setResultValue(array[i+1]);
+                            retorno.setSearchResult(SearchResult.FoundLess);
+                            return retorno;
+                        }
+                    }
+                }
+                if (type== SearchType.LessThan){
+                    if(array[i] < key) {
+                        retorno.setResultIndex(i);
+                        retorno.setResultValue(array[i]);
+                        retorno.setSearchResult(SearchResult.FoundLess);
+                        return retorno;
+                    }
+                }
+
+                if (type== SearchType.GreaterThan) {
+
+                    if(array[i] > key ){
+                        retorno.setResultIndex(i);
+                        retorno.setResultValue(array[i]);
+                        retorno.setSearchResult(SearchResult.FoundGreater);
+                        return retorno;
+                    }
+                }
+            }
+
+
         }
 
         retorno.setSearchResult(SearchResult.NotFound);
--- test/BuscaBinariaTest.java
+++ test/BuscaBinariaTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 public class BuscaBinariaTest {
 
     @Test
-    public void testeBusca0() throws Exception {
+    public void testeBuscaAsc0() throws Exception {
 
         int[] teste = {0, 2, 4, 6, 8};
         int key = -1;
@@ -23,7 +23,7 @@ public class BuscaBinariaTest {
     }
 
     @Test
-    public void testeBusca1() throws Exception {
+    public void testeBuscaAsc1() throws Exception {
 
         int[] teste = {0, 2, 4, 6, 8};
         int key = 0;
@@ -40,7 +40,7 @@ public class BuscaBinariaTest {
     }
 
     @Test
-    public void testeBusca2() throws Exception {
+    public void testeBuscaAsc2() throws Exception {
 
         int[] teste = {0, 2, 4, 6, 8};
         int key = 0;
@@ -57,7 +57,7 @@ public class BuscaBinariaTest {
     }
 
     @Test
-    public void testeBusca3() throws Exception {
+    public void testeBuscaAsc3() throws Exception {
 
         int[] teste = {0, 2, 4, 6, 8};
         int key = 1;
@@ -74,7 +74,7 @@ public class BuscaBinariaTest {
     }
 
     @Test
-    public void testeBusca4() throws Exception {
+    public void testeBuscaAsc4() throws Exception {
 
         int[] teste = {0, 2, 4, 6, 8};
         int key = 2;
@@ -91,7 +91,7 @@ public class BuscaBinariaTest {
     }
 
     @Test
-    public void testeBusca5() throws Exception {
+    public void testeBuscaAsc5() throws Exception {
 
         int[] teste = {0, 2, 4, 6, 8};
         int key = 3;
@@ -107,7 +107,7 @@ public class BuscaBinariaTest {
         assertEquals(resultadoEsperado.getResultValue(),buscaResultado.getResultValue());
     }
     @Test
-    public void testeBusca6() throws Exception {
+    public void testeBuscaAsc6() throws Exception {
 
         int[] teste = {0, 2, 4, 6, 8};
         int key = 2;
@@ -124,7 +124,7 @@ public class BuscaBinariaTest {
     }
 
     @Test
-    public void testeBusca7() throws Exception {
+    public void testeBuscaAsc7() throws Exception {
 
         int[] teste = {0, 2, 4, 6, 8};
         int key = 2;
@@ -141,7 +141,7 @@ public class BuscaBinariaTest {
     }
 
     @Test
-    public void testeBusca8() throws Exception {
+    public void testeBuscaAsc8() throws Exception {
 
         int[] teste = {0, 2, 4, 6, 8};
         int key = 1;
@@ -158,7 +158,7 @@ public class BuscaBinariaTest {
     }
 
     @Test
-    public void testeBusca9() throws Exception {
+    public void testeBuscaAsc9() throws Exception {
 
         int[] teste = {0, 2, 4, 6, 8};
         int key = 2;
@@ -166,6 +166,192 @@ public class BuscaBinariaTest {
         boolean ascending = true;
         SearchType sType = SearchType.LessThan;
         ResultType resultadoEsperado = new ResultType(SearchResult.FoundLess, 0,0);
+        Busca busca = new Busca();
+        ResultType buscaResultado = busca.busca(key,teste,size,ascending,sType);
+
+        assertEquals(resultadoEsperado.getSearchResult(),buscaResultado.getSearchResult());
+        assertEquals(resultadoEsperado.getResultIndex(),buscaResultado.getResultIndex());
+        assertEquals(resultadoEsperado.getResultValue(),buscaResultado.getResultValue());
+    }
+
+    @Test
+    public void testeBuscaDesc0() throws Exception {
+
+        int[] teste = {8, 6, 4, 2, 0};
+        int key = -1;
+        int size = teste.length;
+        boolean ascending = false;
+        SearchType sType = SearchType.LessThan;
+        ResultType resultadoEsperado = new ResultType(SearchResult.NotFound, null, null);
+        Busca busca = new Busca();
+        ResultType buscaResultado = busca.busca(key,teste,size,ascending,sType);
+
+        assertEquals(resultadoEsperado.getSearchResult(),buscaResultado.getSearchResult());
+        assertEquals(resultadoEsperado.getResultIndex(),buscaResultado.getResultIndex());
+        assertEquals(resultadoEsperado.getResultValue(),buscaResultado.getResultValue());
+    }
+
+    @Test
+    public void testeBuscaDesc1() throws Exception {
+
+        int[] teste = {8, 6, 4, 2, 0};
+        int key = 4;
+        int size = teste.length;
+        boolean ascending = false;
+        SearchType sType = SearchType.LessThanEquals;
+        ResultType resultadoEsperado = new ResultType(SearchResult.FoundExact, 2, 4);
+        Busca busca = new Busca();
+        ResultType buscaResultado = busca.busca(key,teste,size,ascending,sType);
+
+        assertEquals(resultadoEsperado.getSearchResult(),buscaResultado.getSearchResult());
+        assertEquals(resultadoEsperado.getResultIndex(),buscaResultado.getResultIndex());
+        assertEquals(resultadoEsperado.getResultValue(),buscaResultado.getResultValue());
+    }
+
+    @Test
+    public void testeBuscaDesc2() throws Exception {
+
+        int[] teste = {8, 6, 4, 2, 0};
+        int key = 8;
+        int size = teste.length;
+        boolean ascending = false;
+        SearchType sType = SearchType.Equals;
+        ResultType resultadoEsperado = new ResultType(SearchResult.FoundExact, 0, 8);
+        Busca busca = new Busca();
+        ResultType buscaResultado = busca.busca(key,teste,size,ascending,sType);
+
+        assertEquals(resultadoEsperado.getSearchResult(),buscaResultado.getSearchResult());
+        assertEquals(resultadoEsperado.getResultIndex(),buscaResultado.getResultIndex());
+        assertEquals(resultadoEsperado.getResultValue(),buscaResultado.getResultValue());
+    }
+
+    @Test
+    public void testeBuscaDesc3() throws Exception {
+
+        int[] teste = {8, 6, 4, 2, 0};
+        int key = 5;
+        int size = teste.length;
+        boolean ascending = false;
+        SearchType sType = SearchType.GreaterThanEquals;
+        ResultType resultadoEsperado = new ResultType(SearchResult.FoundGreater, 1, 6);
+        Busca busca = new Busca();
+        ResultType buscaResultado = busca.busca(key,teste,size,ascending,sType);
+
+        assertEquals(resultadoEsperado.getSearchResult(),buscaResultado.getSearchResult());
+        assertEquals(resultadoEsperado.getResultIndex(),buscaResultado.getResultIndex());
+        assertEquals(resultadoEsperado.getResultValue(),buscaResultado.getResultValue());
+    }
+
+    @Test
+    public void testeBuscaDesc4() throws Exception {
+
+        int[] teste = {8, 6, 4, 2, 0};
+        int key = 2;
+        int size = teste.length;
+        boolean ascending = false;
+        SearchType sType = SearchType.GreaterThanEquals;
+        ResultType resultadoEsperado = new ResultType(SearchResult.FoundExact, 3, 2);
+        Busca busca = new Busca();
+        ResultType buscaResultado = busca.busca(key,teste,size,ascending,sType);
+
+        assertEquals(resultadoEsperado.getSearchResult(),buscaResultado.getSearchResult());
+        assertEquals(resultadoEsperado.getResultIndex(),buscaResultado.getResultIndex());
+        assertEquals(resultadoEsperado.getResultValue(),buscaResultado.getResultValue());
+    }
+
+    @Test
+    public void testeBuscaDesc5() throws Exception {
+
+        int[] teste = {8, 6, 4, 2, 0};
+        int key = 9;
+        int size = teste.length;
+        boolean ascending = false;
+        SearchType sType = SearchType.GreaterThan;
+        ResultType resultadoEsperado = new ResultType(SearchResult.NotFound, null,null);
+        Busca busca = new Busca();
+        ResultType buscaResultado = busca.busca(key,teste,size,ascending,sType);
+
+        assertEquals(resultadoEsperado.getSearchResult(),buscaResultado.getSearchResult());
+        assertEquals(resultadoEsperado.getResultIndex(),buscaResultado.getResultIndex());
+        assertEquals(resultadoEsperado.getResultValue(),buscaResultado.getResultValue());
+    }
+    @Test
+    public void testeBuscaDesc6() throws Exception {
+
+        int[] teste = {8, 6, 4, 2, 0};
+        int key = 3;
+        int size = teste.length;
+        boolean ascending = false;
+        SearchType sType = SearchType.Equals;
+        ResultType resultadoEsperado = new ResultType(SearchResult.NotFound, null,null);
+        Busca busca = new Busca();
+        ResultType buscaResultado = busca.busca(key,teste,size,ascending,sType);
+
+        assertEquals(resultadoEsperado.getSearchResult(),buscaResultado.getSearchResult());
+        assertEquals(resultadoEsperado.getResultIndex(),buscaResultado.getResultIndex());
+        assertEquals(resultadoEsperado.getResultValue(),buscaResultado.getResultValue());
+    }
+
+    @Test
+    public void testeBuscaDesc7() throws Exception {
+
+        int[] teste = {8, 6, 4, 2, 0};
+        int key = -1;
+        int size = teste.length;
+        boolean ascending = false;
+        SearchType sType = SearchType.LessThanEquals;
+        ResultType resultadoEsperado = new ResultType(SearchResult.NotFound, null,null);
+        Busca busca = new Busca();
+        ResultType buscaResultado = busca.busca(key,teste,size,ascending,sType);
+
+        assertEquals(resultadoEsperado.getSearchResult(),buscaResultado.getSearchResult());
+        assertEquals(resultadoEsperado.getResultIndex(),buscaResultado.getResultIndex());
+        assertEquals(resultadoEsperado.getResultValue(),buscaResultado.getResultValue());
+    }
+
+    @Test
+    public void testeBuscaDesc8() throws Exception {
+
+        int[] teste = {8, 6, 4, 2, 0};
+        int key = 7;
+        int size = teste.length;
+        boolean ascending = false;
+        SearchType sType = SearchType.GreaterThan;
+        ResultType resultadoEsperado = new ResultType(SearchResult.FoundGreater, 0,8);
+        Busca busca = new Busca();
+        ResultType buscaResultado = busca.busca(key,teste,size,ascending,sType);
+
+        assertEquals(resultadoEsperado.getSearchResult(),buscaResultado.getSearchResult());
+        assertEquals(resultadoEsperado.getResultIndex(),buscaResultado.getResultIndex());
+        assertEquals(resultadoEsperado.getResultValue(),buscaResultado.getResultValue());
+    }
+
+    @Test
+    public void testeBuscaDesc9() throws Exception {
+
+        int[] teste = {8, 6, 4, 2, 0};
+        int key = 1;
+        int size = teste.length;
+        boolean ascending = false;
+        SearchType sType = SearchType.LessThanEquals;
+        ResultType resultadoEsperado = new ResultType(SearchResult.FoundLess, 4,0);
+        Busca busca = new Busca();
+        ResultType buscaResultado = busca.busca(key,teste,size,ascending,sType);
+
+        assertEquals(resultadoEsperado.getSearchResult(),buscaResultado.getSearchResult());
+        assertEquals(resultadoEsperado.getResultIndex(),buscaResultado.getResultIndex());
+        assertEquals(resultadoEsperado.getResultValue(),buscaResultado.getResultValue());
+    }
+
+    @Test
+    public void testeBuscaDesc10() throws Exception {
+
+        int[] teste = {8, 6, 4, 2, 0};
+        int key = 4;
+        int size = teste.length;
+        boolean ascending = false;
+        SearchType sType = SearchType.LessThan;
+        ResultType resultadoEsperado = new ResultType(SearchResult.FoundLess, 4,0);
         Busca busca = new Busca();
         ResultType buscaResultado = busca.busca(key,teste,size,ascending,sType);
 
